8b3bc5ea406f2a56d9e7c63bc1179b8927379a6f
Sonalika Pradhan <sonalikap2003@gmail.com>
2025-08-06 23:41:22 +0530

    Fix: update ImageKit config with correct env setup

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -2,7 +2,7 @@
 
 import ImageKit from "imagekit";
 
-const imageKit = new ImageKit({
+var imageKit = new ImageKit({
   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
