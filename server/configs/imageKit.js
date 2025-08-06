c96f9715a83d3aa8267aee0e7aeaf54da51dae2a
Sonalika Pradhan <sonalikap2003@gmail.com>
2025-08-06 23:58:24 +0530

    Add ImageKit config file

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -2,7 +2,7 @@
 
 import ImageKit from "imagekit";
 
-var imageKit = new ImageKit({
+const imageKit = new ImageKit({
   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
